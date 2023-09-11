c7a89603c91c8358c37dbe0b80a80a9b7bc22ec7
Lulit1 <134982488+Lulit1@users.noreply.github.com>
2023-09-11 13:48:42 -0400

    last update

--- client/src/utility/axios.jsx
+++ client/src/utility/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://localhost:4500", // api url
-	// baseURL: "https://evangadifinal.onrender.com/", // api url
+	// baseURL: "http://localhost:4500", // api url
+	baseURL: "https://evangadi-forum-axzr.onrender.com",
 
 	withCredentials: true,
 });
